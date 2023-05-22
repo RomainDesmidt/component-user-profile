f8ce28ccb003e88b09073e66f105bfe4640d4c04
Romain DESMIDT <rdesmidt47@gmail.com>
2023-05-22 13:29:21 +0200

    added formsmodule ref + userprofile binding

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,18 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
-  name: string;
-  firstname: string;
-  age: number;
-  quote: string;
-  photo: string;
+  name: string = "Doe";
+  firstname: string = "John";
+  age: number = 25;
+  quote: string = "Avada Kedavra";
+  photo: string = "https://randomuser.me/api/portraits/lego/2.jpg";
+  clicked: boolean = false;
 
-  constructor(nameValu: string, firstnameValue: string, ageValue: number, quoteValue: string ) {
-    this.name = nameValu;
-    this.firstname = firstnameValue;
-    this.age = ageValue;
-    this.quote = quoteValue;
-    this.photo = 'https://randomuser.me/api/portraits/lego/2.jpg';
+  constructor() { }
+
+  onClicked() : void {
+    this.clicked = !this.clicked
   }
+
+  ngOnInit(): void {
+  }
+
+  
+  // name: string;
+  // firstname: string;
+  // age: number;
+  // quote: string;
+  // photo: string;
+
+  // constructor(@Inject(String)  nameValu: string, firstnameValue: string, ageValue: number, quoteValue: string ) {
+  //   this.name = nameValu;
+  //   this.firstname = firstnameValue;
+  //   this.age = ageValue;
+  //   this.quote = quoteValue;
+  //   this.photo = 'https://randomuser.me/api/portraits/lego/2.jpg';
+  // }
 
 }
